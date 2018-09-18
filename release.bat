d0f0218b4858ced4d3728d0b1155763c12d606c3
Zae <zaevi@live.com>
2018-09-18 09:09:26 +0800

    ignore release.bat

--- release.bat
+++
@@ -1,5 +0,0 @@
-call C:\Dev\VS2017\Community\Common7\Tools\VsDevCmd.bat;
-msbuild QuickRun.sln /p:Configuration=Release
-mkdir Release
-copy /y QuickRun\bin\Release\QuickRun.exe Release\QuickRun.exe
-copy /y QuickRun.Setting\bin\Release\QuickRun.Setting.exe Release\QuickRun.Setting.exe
