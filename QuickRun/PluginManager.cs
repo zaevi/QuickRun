08c5408b5caba308b6ebb4d61bc268b5dcbaa265
Zae <zaevi@live.com>
2018-10-02 00:07:05 +0800

    移动文件

--- QuickRun/PluginManager.cs
+++ QuickRun/PluginManager.cs
@@ -3,9 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using System.Windows;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace QuickRun
 {
