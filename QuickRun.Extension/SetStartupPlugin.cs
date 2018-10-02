0c55cb13c885723e8c12aab0fc0ac11179a84d71
Zae <526131722@qq.com>
2018-10-02 15:43:58 +0800

    Add: 开机启动选项

---
+++ QuickRun.Extension/SetStartupPlugin.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using QuickRun.Plugin;
+using System.Windows.Forms;
+
+namespace QuickRun.Extension
+{
+    [Plugin("$SetStartup", "设置开机启动")]
+    class SetStartupPlugin : NoDragPlugin
+    {
+        const string StartupSubKey = @"Software\Microsoft\Windows\CurrentVersion\Run";
+
+        const string AppName = @"QuickRun";
+
+        readonly string AppPath = typeof(QuickRun.App).Assembly.Location;
+
+        public override void Execute()
+        {
+            var key = Registry.CurrentUser.OpenSubKey(StartupSubKey, true);
+
+            if(key.GetValue(AppName) == null)
+            {
+                key.SetValue(AppName, AppPath);
+                MessageBox.Show("已设为开机启动", "QuickRun");
+            }
+            else
+            {
+                key.DeleteValue(AppName, false);
+                MessageBox.Show("已取消开机启动", "QuickRun");
+            }
+        }
+    }
+}
