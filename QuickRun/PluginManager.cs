eac580015e964357ecdf76d319eb504cf1653c7a
Zae <zaevi@live.com>
2018-10-15 15:22:48 +0800

    非标准插件支持

--- QuickRun/PluginManager.cs
+++ QuickRun/PluginManager.cs
@@ -1,6 +1,7 @@
 ﻿using QuickRun.Plugin;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 using System.Windows;
@@ -11,7 +12,7 @@ namespace QuickRun
     {
 
         static HashSet<string> LoadedAssembly = new HashSet<string>();
-        static Dictionary<string, Type> PluginMap = new Dictionary<string, Type>();
+        static Dictionary<string, object> PluginMap = new Dictionary<string, object>();
 
         public static bool LoadPlugin(string path)
         {
@@ -29,22 +30,34 @@ namespace QuickRun
                         string key = pType.GetCustomAttribute<PluginAttribute>()?.Key ?? ("$" + pType.FullName);
                         PluginMap[key] = pType;
                     }
-                    LoadedAssembly.Add(assembly.FullName);
-                    return true;
                 }
                 else
                 {
-                    throw new Exception("not support"); // but soon
+                    var types = assembly.ExportedTypes.Where(t => t.Name.EndsWith(@"QuickRun") || t.Name.EndsWith(@"QuickRunPlugin"));
+                    foreach(var pType in types)
+                    {
+                        var method = pType.GetMethod("Execute", BindingFlags.Public | BindingFlags.Static);
+                        if (method is null) continue;
+                        var pObj = new NonStandardPlugin(method);
+                        var key = pType.GetCustomAttribute<DisplayNameAttribute>()?.DisplayName
+                            ?? pType.FullName.Substring(0, pType.FullName.LastIndexOf("QuickRun"));
+                        key = "$" + key.TrimStart('$');
+                        PluginMap[key] = pObj;
+                    }
                 }
+                LoadedAssembly.Add(assembly.FullName);
+                return true;
             }
             catch { return false; }
         }
 
         public static bool ExecutePlugin(Item item)
         {
-            if (!PluginMap.TryGetValue(item.Key, out var pType))
+            if (!PluginMap.TryGetValue(item.Key, out var pObj))
                 return false;
-            var iplugin = Activator.CreateInstance(pType) as IPlugin;
+            IPlugin iplugin;
+            if (pObj is Type) iplugin = Activator.CreateInstance(pObj as Type) as IPlugin;
+            else iplugin = pObj as IPlugin;
             if(iplugin is Plugin.Plugin plugin)
                 plugin.Arguments = item.Arguments;
             iplugin.Execute();
@@ -54,9 +67,11 @@ namespace QuickRun
         public static bool ExecutePlugin(Item item, IDataObject dragData=null)
         {
             if (dragData == null) return ExecutePlugin(item);
-            if (!PluginMap.TryGetValue(item.Key, out var pType))
+            if (!PluginMap.TryGetValue(item.Key, out var pObj))
                 return false;
-            var iplugin = Activator.CreateInstance(pType) as IPlugin;
+            IPlugin iplugin;
+            if (pObj is Type) iplugin = Activator.CreateInstance(pObj as Type) as IPlugin;
+            else iplugin = pObj as IPlugin;
             if (iplugin is Plugin.Plugin plugin)
             {
                 plugin.Arguments = item.Arguments;
@@ -66,6 +81,21 @@ namespace QuickRun
                 return false;
             iplugin.Execute();
             return true;
+        }
+    }
+
+    class NonStandardPlugin : FileDropPlugin
+    {
+        MethodInfo Method = null;
+
+        public NonStandardPlugin(MethodInfo method)
+        {
+            Method = method;
+        }
+
+        public override void Execute()
+        {
+            Method?.Invoke(null, null);
         }
     }
 }
